438886406c01278f1d87e1843bafcde7a88d832e
Valentin Andujar <valentin_andujar@hotmail.com>
2022-10-03 18:19:08 +0200

    get links from a page, not from page 2 if there is one
    get extraction with csv file for one book

--- projet01_OC.py
+++ projet01_OC.py
@@ -1,40 +1,87 @@
+import csv
 from pprint import pprint
-import re
 
 import requests
 from bs4 import BeautifulSoup
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Max-Age': '3600',
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
+import pandas
 
-for i in range(11):
+url = "http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
 
-    url = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-    req = requests.get(url, headers)
-
-
-    soup = BeautifulSoup( req.content, 'html.parser' )
-    links_url = soup.find_all("h3")
-    links_url = str(links_url)
-    links_url = re.split(r' href="| title=',links_url)
-    links_url = [str(link).replace("../../../","http://books.toscrape.com/catalogue/").replace('"','') for link in links_url if link.startswith("../../../")]
-    links_url =links_url[i]
-    print(links_url)
-
-    if req.ok:
-        pass
-
-
-
-# def extraction():
-#     pass
+# links=[]
+# link_soup = soup.find_all("h3")
 #
-# def transformation():
-#     pass
+# for link in link_soup:
+#     a = link.find('a')
+#     link = a["href"]
+#     links.append(link.replace("../../../","http://books.toscrape.com/" ))
+
+def extract(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("problème d'url")
+    # print(r.status_code)
+
+    soup = BeautifulSoup( r.content, 'html.parser' )
+
+    return soup
+
+
+def transform(soup):
+    key = []
+    value = []
+
+    data_value= soup.find_all('td')
+    for i in range(6):
+        value.append(data_value[i].text)
+
+    data_key = soup.find_all('th')
+
+    for i in range(6):
+        key.append(data_key[i].text)
+
+    h1 = soup.find("h1").text
+    value.append(h1)
+    key.append("Title")
+    # print(key)
+    # print( value )
+
+    data_rating = soup.find('p' ,{"class":"star-rating Two"})
+    data_rating = data_rating["class"]
+
+    key.append(data_rating[0])
+    value.append(data_rating[1])
+
+    # return data
+    data_cat = soup.find_all( 'a' )
+    data_cat = data_cat[3]
+    data_cat = data_cat.string
+    key.append("categories")
+    value.append(data_cat)
+
+
+    data_image_url = soup.find_all('img')
+    data_image_url = data_image_url[0].get("src")
+    key.append("image_url")
+    value.append("http://books.toscrape.com/"+data_image_url)
+
+    data_description = soup.find_all('p')
+    data_description = data_description[3].text
+
+    key.append(soup.h2.text)
+    value.append(data_description)
+    print(key)
+    return key, value
+
+def loading(line_1, line_2):
+    with open( "test.csv", "w", newline="" ) as csv_file:
+        writer = csv.writer( csv_file, delimiter="," )
+        writer.writerow( line_1 )
+        writer.writerow( line_2 )
+
+soup = extract( url )
 #
-# def loading():
-#     pass
+line_1,line_2 = transform( soup )
+loading(line_1,line_2)
+
+
+
