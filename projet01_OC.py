f8885187ec0ecbb40a2b2a87d2e266808c222b3b
Valentin Andujar <valentin_andujar@hotmail.com>
2022-10-21 08:22:11 +0200

    header of csv files added

--- projet01_OC.py
+++ projet01_OC.py
@@ -1,4 +1,6 @@
 import csv
+import logging
+import os
 import re
 import string
 import urllib.request
@@ -10,9 +12,15 @@ from bs4 import BeautifulSoup
 
 from url import url_books
 
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / 'projet01_OC.log', level=logging.INFO)
+
+
+
 for url in range( len(url_books) ):  # len(url_books)
 
-    def extract(url_books):
+
+    def extract( url_books ):
         """scrape the page web of books
 
         Args:
@@ -24,16 +32,14 @@ for url in range( len(url_books) ):  # len(url_books)
         """
         r = requests.get( url_books )
         if r.status_code != 200:
-            print( "problème d'url" )
-
+            logging.debug(f"PROBLEME URL : {r.status_code}")
         soup = BeautifulSoup( r.content, 'html.parser' )
 
         return soup
 
+
     def transform(soup, url):
-        """get datas for books (product_page_url", "universal_product_code (upc)", "title", "price_including_tax",
-                  "price_excluding_tax", "number_available", "product_description", "category", "review_rating",
-                  "image_url)
+        """get datas for books
         Args:
             soup(soup):scraped web page
             url(str):url of a book
@@ -48,9 +54,11 @@ for url in range( len(url_books) ):  # len(url_books)
                   "image_url"]
 
         h1 = soup.find( "h1" ).text
+        h1 = "".join( [i.replace(" ","_") for i in h1 if i not in string.punctuation ] )
         data_value = soup.find_all( 'td' )
         for i in range( 6 ):
             value.append( data_value[i].text )
+            logging.info( f"data_value[i].text : {data_value[i].text}" )
             if i == 0:
                 value.append( h1 )
             else:
@@ -66,7 +74,7 @@ for url in range( len(url_books) ):  # len(url_books)
         data_cat = soup.find_all( 'a' )
         data_cat = data_cat[3]
         data_cat = data_cat.string
-
+        logging.info( f"data_cat : {data_cat}" )
         value.append( data_cat )
 
         a = soup.find( 'p', {'class': re.compile( r'star-rating.*' )} )
@@ -96,10 +104,9 @@ for url in range( len(url_books) ):  # len(url_books)
 
         with open( line_1[7] + ".csv", "a", newline="", encoding="utf-8" ) as csv_file:
             writer = csv.writer( csv_file, delimiter="," )
-            if url == 0:
+            file_is_empty = os.stat(line_1[7] + ".csv").st_size == 0
+            if file_is_empty:
                 writer.writerow( header )
-            else:
-                pass
             writer.writerow( line_1 )
 
 
@@ -110,17 +117,14 @@ for url in range( len(url_books) ):  # len(url_books)
             line_1(list(str)): data from books
 
         """
-
         link = line_1[9].replace( "../../", "" )
-        print( link )
         title = line_1[2]
-        title = "".join( [i.rstrip() for i in title if i not in string.punctuation] )
+        title = "".join( [i for i in title.replace(" ", "_")  ] )
         print( title )
         cat = line_1[7]
-        print( cat )
         list_zip = (title, cat)
         print( list_zip )
-        urllib.request.urlretrieve( link, title )
+        urllib.request.urlretrieve(link, title)
         img = Image.open( title )
         img = img.save( title + ".png" )
 
@@ -137,10 +141,10 @@ for url in range( len(url_books) ):  # len(url_books)
 
             a = [f.unlink() for f in BASE_DIR.iterdir() if f.is_file() and f.suffix == ""]
         except FileExistsError:
-            print( "Files already created" )
+            logging.info( "Files already created" )
             pass
         except FileNotFoundError:
-            print( "path no found" )
+            logging.info( "path no found" )
             pass
 
 
@@ -148,6 +152,7 @@ for url in range( len(url_books) ):  # len(url_books)
     header, line_1 = transform( soup, url_books[url] )
     loading( header, line_1 )  # header, line_1
     get_pictures( line_1 )
+
 
 # creates a folder for the .csv files and stores them in it
 try:
@@ -159,8 +164,9 @@ try:
 
     for file in files:
         starget_file = file.stem
-        print( type( file.stem ) )
-        print( file.stem )
+        # print( type( file.stem ) )
+
+        logging.info( f"Files name:{file.stem}" )
         absolut_starget_folder = filepath / starget_file
         absolut_starget_folder.mkdir( exist_ok=True )
         file_cible = absolut_starget_folder / file.name
@@ -169,5 +175,7 @@ try:
         file.rename( file_cible )
 
 except FileExistsError:
-    print( "Files already created" )
+    logging.info( "Files already created" )
     pass
+
+
