502c463ea561b7067dd39d35a78acc6283b206da
Valentin Andujar <valentin_andujar@hotmail.com>
2022-11-11 09:00:04 +0100

    pep8 corrections, pep8 corrections, change of place of csv files during recovery

--- projet01_OC.py
+++ projet01_OC.py
@@ -4,19 +4,24 @@ import os
 import re
 import string
 import urllib.request
+
 from pathlib import Path
+
 
 import requests
 from PIL import Image
+
 from bs4 import BeautifulSoup
 
 from url import url_books
 
-BASE_DIR = Path( __file__ ).resolve().parent
-logging.basicConfig( filename=BASE_DIR / 'projet01_OC.log', level=logging.INFO )
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / 'projet01_OC.log', level=logging.INFO)
 
-for url in range( len(url_books) ):  # len(url_books)
-    print(len( url_books ))
+for url in range(len(url_books)):  # len(url_books)
+    logging.info(f"number books {len(url_books)}")
+
+
     def extract(books):
         """scrape the page web of books
 
@@ -27,10 +32,10 @@ for url in range( len(url_books) ):  # len(url_books)
             scraped web page
 
         """
-        r = requests.get( books )
+        r = requests.get(books)
         if r.status_code != 200:
-            logging.debug( f"PROBLEME URL : {r.status_code}" )
-        beauti = BeautifulSoup( r.content, 'html.parser' )
+            logging.debug(f"PROBLEME URL : {r.status_code}")
+        beauti = BeautifulSoup(r.content, 'html.parser')
 
         return beauti
 
@@ -45,50 +50,50 @@ for url in range( len(url_books) ):  # len(url_books)
             all datas for books
 
         """
-        csv_header = ("product page u", "universal product code (upc)", "title",
+        csv_header = ("product page url", "Universal Product Code (U.P.C)", "title",
                       "price excluding tax", "price including tax", "number available",
-                      "product description", "category", "review rating", "image u")
+                      "product description", "category", "review rating", "image url")
         value = [u]
 
-        h1 = s.find( "h1" ).text
-        h1 = "".join( [i for i in h1 if i not in string.punctuation] )
-        data_value = s.find_all( 'td' )
+        h1 = s.find("h1").text
+        h1 = "".join([i for i in h1 if i not in string.punctuation])
+        data_value = s.find_all('td')
         upc = data_value[0].text
-        value.append( upc )
-        value.append( h1 )
+        value.append(upc)
+        value.append(h1)
         price_exclu_tax = data_value[2].text
         price_inclu_tax = data_value[3].text
-        value.append( price_exclu_tax )
-        value.append( price_inclu_tax )
+        value.append(price_exclu_tax)
+        value.append(price_inclu_tax)
         stock = data_value[5].text
         stock = [i for i in stock if i.isdigit()]
-        stock = "".join( stock )
-        value.append( stock )
-        data_description = s.find_all( 'p' )
+        stock = "".join(stock)
+        value.append(stock)
+        data_description = s.find_all('p')
 
         data_description = data_description[3].text
         value.append(data_description)
 
-        data_cat = s.find_all( 'a' )
+        data_cat = s.find_all('a')
         data_cat = data_cat[3]
         data_cat = data_cat.string
-        logging.info( f"data_cat : {data_cat}" )
-        value.append( data_cat )
+        logging.info(f"data_cat : {data_cat}")
+        value.append(data_cat)
 
-        rating = s.find( 'p', {'class': re.compile( r'star-rating.*' )} )
+        rating = s.find('p', {'class': re.compile(r'star-rating.*')})
         rating = rating["class"]
 
-        value.append( rating[1] )
+        value.append(rating[1])
 
-        data_image_url = s.find_all( 'img' )
-        data_image_url = data_image_url[0].get( "src" )
+        data_image_url = s.find_all('img')
+        data_image_url = data_image_url[0].get("src")
 
         value.append(f"http://books.toscrape.com/{data_image_url}")
 
         return csv_header, value
 
 
-    def loading(a, b):  # a, data_book
+    def loading(a, b):
         """write on the csv file
 
         Args:
@@ -97,28 +102,30 @@ for url in range( len(url_books) ):  # len(url_books)
 
         Returns:
             csv files with datas from books
-
         """
+        folder = Path.cwd() / "DATA" / f"{b[7]}"
+        folder.mkdir(exist_ok=True, parents=True)
+        file = Path.cwd() / "DATA" / f"{b[7]}" / f"{b[7]}.csv"
+        file.touch()
 
-        with open( b[7] + ".csv", "a", newline="", encoding="utf-8" ) as csv_file:
-            writer = csv.writer( csv_file, delimiter="," )
-            file_is_empty = os.stat(f"{b[7]}.csv" ).st_size == 0
+        with open(file, "a", newline="", encoding="utf-8") as csv_file:
+            writer = csv.writer(csv_file, delimiter=",")
+            file_is_empty = os.stat(file).st_size == 0
             if file_is_empty:
-                writer.writerow( a )
-            writer.writerow( b )
+                writer.writerow(a)
+            writer.writerow(b)
 
 
-    # noinspection PyShadowingNames
-    def get_pictures(data_book):
+    def download_pictures(data_book):
         """dowload book pictures and creating of data folder, categorie folder and book folder
 
         Args:
             data_book(list(str)): data from books
 
         """
-        link = data_book[9].replace( "../../", "" )
+        link = data_book[9].replace("../../", "")
         title = data_book[2]
-        title = "".join( [i for i in title] )
+        title = "".join([i for i in title])
         cat = data_book[7]
         urllib.request.urlretrieve( link, title )
         img = Image.open( title )
@@ -131,40 +138,20 @@ for url in range( len(url_books) ):  # len(url_books)
 
             for f in files_png:
                 output_dir = base_dir / "DATA" / cat / title
-                output_dir.mkdir( exist_ok=True, parents=True )
+                output_dir.mkdir(exist_ok=True, parents=True)
 
-                f.rename( output_dir / f.name )
+                f.rename(output_dir / f.name)
 
             [f.unlink() for f in base_dir.iterdir() if f.is_file() and f.suffix == ""]
         except FileExistsError:
-            logging.info( "Files already created" )
+            logging.info("Files already created")
 
         except FileNotFoundError:
-            logging.info( "path no found" )
+            logging.info("path no found")
 
 
     soup = extract(url_books[url])
     header, line_1 = transform(soup, url_books[url])
-    loading(header, line_1)  # a, data_book
-    get_pictures(line_1)
+    loading(header, line_1)
+    download_pictures(line_1)
 
-# creates a folder for the .csv files and stores them in it
-try:
-    BASE_DIR = Path.cwd()
-    filepath = Path( BASE_DIR / "DATA" )
-    filepath.mkdir(exist_ok=True)
-
-    files = [f for f in BASE_DIR.iterdir() if f.is_file() and f.suffix == ".csv"]
-
-    for file in files:
-        starget_file = file.stem
-
-        logging.info( f"Files name:{file.stem}" )
-        absolut_starget_folder = filepath / starget_file
-        absolut_starget_folder.mkdir( exist_ok=True )
-        file_cible = absolut_starget_folder / file.name
-
-        file.rename( file_cible )
-
-except FileExistsError:
-    logging.debug( "Files already created" )
--- url.py
+++ url.py
@@ -1,12 +1,12 @@
 import logging
 import re
+from pathlib import Path
 
 import bs4
 import requests
-from pathlib import Path
 
-BASE_DIR = Path( __file__ ).resolve().parent
-logging.basicConfig( filename=BASE_DIR / 'link.log', level=logging.INFO )
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / 'link.log', level=logging.INFO)
 
 
 def get_url_index():
@@ -17,13 +17,14 @@ def get_url_index():
 
     """
 
-    r = requests.get( "http://books.toscrape.com/catalogue/category/books_1/index.html" )
+    r = requests.get("http://books.toscrape.com/catalogue/category/books_1/index.html")
 
-    soup = bs4.BeautifulSoup( r.content, 'html.parser' )
-    links = [str( link.get( 'href' ) ) for link in soup.find_all( 'a' )]
+    soup = bs4.BeautifulSoup(r.content, 'html.parser')
+    links = [str(link.get('href')) for link in soup.find_all('a')]
     links = links[3:53]  # 53
-    list_url_index = [link.replace( "../", "http://books.toscrape.com/catalogue/category/" ) for link in links]
-    logging.info( f"Generating index link : {list_url_index}, link number = {len( list_url_index )}" )
+    list_url_index = [link.replace("../", "http://books.toscrape.com/catalogue/category/") for link in links]
+    logging.info(f"Generating index link : {list_url_index}, link number = {len(list_url_index)}")
+
     return list_url_index
 
 
@@ -34,29 +35,28 @@ def get_url_page():  # (no books)
         (list[str]): list that contains all urls pages of categories
 
     """
-
     all_url_without_books = []
-    for i in range( 50 ):  # 50
-        a = re.match( r".+(_\d|\d([/index.html]))", url_index[i] )
+    for i in range(50):  # 50
+        a = re.match(r".+(_\d|\d([/index.html]))", url_index[i])
         if a is not None:
-            all_url_without_books.append( url_index[i] )
+            all_url_without_books.append(url_index[i])
 
-        for page in range( 1, 9 ):  # min one page and max 8 pages for one categorie
-            url_tested = url_index[i].replace( "index.html", f"page-{page}.html" )
+        for page in range(1, 9):  # min one page and max 8 pages for one categorie
+            url_tested = url_index[i].replace("index.html", f"page-{page}.html")
 
-            all_url_without_books.append( url_tested )
+            all_url_without_books.append(url_tested)
     urls_clean = []
     for url in all_url_without_books:
-        r = requests.get( url )
+        r = requests.get(url)
         if r.ok:
-            urls_clean.append( url )
-    urls_clean = sorted( list( set( urls_clean ) ) )
+            urls_clean.append(url)
+    urls_clean = sorted(list(set(urls_clean)))
 
     for url in urls_clean:
-        b = re.match( r".+/page-'1'.html", url )
+        b = re.match(r".+/page-'1'.html", url)
         if b is not None:
-            urls_clean.remove( url )
-    logging.info( f"Generating good u : {urls_clean}, {len( urls_clean )}" )
+            urls_clean.remove(url)
+    logging.info(f"Generating good u : {urls_clean}, {len(urls_clean)}")
     return urls_clean
 
 
@@ -68,30 +68,27 @@ def get_url_book(all_url_wthout_books_clean):
 
     Returns:
         list[str]: u list of books
-
     """
 
     books = []
     for url in all_url_wthout_books_clean:
-        r = requests.get( url )
-        soup = bs4.BeautifulSoup( r.content, 'html.parser' )
-        h3 = soup.find_all( "h3" )
+        r = requests.get(url)
+        soup = bs4.BeautifulSoup(r.content, 'html.parser')
+        h3 = soup.find_all("h3")
         links = []
         for h in h3:
-            a = h.find( 'a' )
+            a = h.find('a')
             link = a["href"]
-            links.append( str( link ) )
+            links.append(str(link))
 
         for i in links:
-            a = i.replace( "../../../", "http://books.toscrape.com/catalogue/" )
+            a = i.replace("../../../", "http://books.toscrape.com/catalogue/")
             if a not in books:
-                books.append( a )
-    logging.info( f"Generating good u : {books}, {len( books )} " )
+                books.append(a)
+    logging.info(f"Generating good u : {books}, {len(books)} ")
     return books
 
 
 url_index = get_url_index()
-
 all_url_without_books_clean = get_url_page()
-
-url_books = get_url_book( all_url_without_books_clean )
+url_books = get_url_book(all_url_without_books_clean)
